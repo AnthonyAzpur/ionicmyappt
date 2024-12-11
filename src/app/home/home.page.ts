494e1ea92b909932b6490efd857b3f5b1d02116c
anthony azpur palomino <anthony12al20@gmail.com>
2024-12-11 16:47:36 -0500

    2comit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { LoadingController, ModalController, Platform } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { BarcodeScanner, LensFacing } from '@capacitor-mlkit/barcode-scanning';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  segment =  'scan';
+  scanResult= '';
+  constructor(
+    private loadingController: LoadingController,
+    private platform : Platform,
+    private modalController: ModalController
 
-  constructor() {}
+
+
+  ) {}
+
+  ngOnInit():void{
+    if(this.platform.is('capacitor')){
+      BarcodeScanner.isSupported().then();
+      BarcodeScanner.checkPermissions().then();
+      BarcodeScanner.removeAllListeners();
+    }
+  }
+
+  async starScan() {
+    const modal = await this.modalController.create({
+    component: BarcodeScanningModalComponent,
+    cssClass:'barcode-scannig-modal',
+    showBackdrop:false,
+    componentProps: { 
+      formats:[],
+      LensFacing:LensFacing.Back
+    }
+    });
+  
+    await modal.present();
+    
+    const {data} =await modal.onWillDismiss();
+
+    if(data){
+      this.scanResult = data?.barcode?.displayValue;
+    }
+
+
+
+  }
+
 
 }
